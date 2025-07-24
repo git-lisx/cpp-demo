c0c16c81afac953ac667eb8748a722dd58fd9748
lishixian <714492291@qq.com>
2025-07-24 10:10:19 +0800

    feat: 增加纯CMakeLists.txt项目和CMakeLists.txt + vcpkg包管理项目的示例代码

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # cpp-demo
+
 C++项目demo
+
+- my_crow_server：纯CMakeLists.txt项目
+- my_vcpkg_crow_server：CMakeLists.txt + vcpkg包管理项目
+
+## vcpkg
+
+链接：https://github.com/microsoft/vcpkg
+安装
+
+```shell
+# 安装
+git clone https://github.com/microsoft/vcpkg.git
+cd vcpkg && ./bootstrap-vcpkg.sh
+
+# 配置环境变量
+vim ~/.bashrc
+export VCPKG_ROOT=/home/utenet/lisx/vcpkg
+export PATH=$VCPKG_ROOT:$PATH
+source ~/.bashrc
+
+```
+
+## 编译自己的项目
+
+```shell
+my_vcpkg_crow_server/
+├── CMakeLists.txt
+├── vcpkg.json
+├── src/
+│   └── server.cpp
+
+
+# 安装依赖
+cd /home/utenet/lisx/my_vcpkg_crow_server
+vcpkg install     # 安装依赖vcpkg.json
+
+mkdir build && cd build
+cmake .. -DCMAKE_TOOLCHAIN_FILE=$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake
+make
+./server
+
+
+```
---
+++ my_crow_server/src/server.cpp
@@ -0,0 +1,34 @@
+#include "crow/app.h"
+#include "nlohmann/json.hpp"
+
+using json = nlohmann::json;
+
+int main()
+{
+    crow::SimpleApp app;
+
+    CROW_ROUTE(app, "/api/test").methods(crow::HTTPMethod::Post)
+    ([](const crow::request& req) {
+        try {
+            auto req_json = json::parse(req.body);
+
+            std::string name = req_json.value("name", "unknown");
+            int age = req_json.value("age", 0);
+
+            json resp_json = {
+                {"status", "success"},
+                {"message", "Hello22, " + name},
+                {"age_plus_one", age + 1}
+            };
+
+            return crow::response(200, resp_json.dump());
+
+        } catch (...) {
+            return crow::response(400, R"({"status":"error","message":"Invalid JSON"})");
+        }
+    });
+
+    std::cout << "Server running at http://localhost:18080/api/test" << std::endl;
+    app.port(18080).multithreaded().run();
+}
+
